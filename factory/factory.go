2824ea10f9be738a51d1fd94535ed41e6610951d
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-08-21 22:24:55 +0300

    📦 NEW: Create getters for client factory

--- factory/factory.go
+++ factory/factory.go
@@ -12,15 +12,27 @@ func (cf *AerospikeClientFactory) SetHostname(hostname string) {
 	cf.hostname = hostname
 }
 
+func (cf *AerospikeClientFactory) GetHostname() string {
+	return cf.hostname
+}
+
 func (cf *AerospikeClientFactory) SetPort(port int) {
 	cf.port = port
+}
+
+func (cf *AerospikeClientFactory) GetPort() int {
+	return cf.port
 }
 
 func (cf *AerospikeClientFactory) SetClientPolicy(policy *aerospike.ClientPolicy) {
 	cf.policy = policy
 }
 
-func (cf *AerospikeClientFactory) GetClient() (*aerospike.Client, aerospike.Error) {
+func (cf *AerospikeClientFactory) GetClientPolicy() *aerospike.ClientPolicy {
+	return cf.policy
+}
+
+func (cf *AerospikeClientFactory) BuildClient() (*aerospike.Client, aerospike.Error) {
 	if cf.policy == nil {
 		return aerospike.NewClient(cf.hostname, cf.port)
 	}
