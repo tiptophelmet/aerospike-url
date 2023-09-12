a7e6aef5fe5f8ee31ecdb268847275d246d975f6
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-09-12 14:51:30 +0200

    👌 IMPROVE: Remove nil-url error instance

--- aerourl/err.go
+++ aerourl/err.go
@@ -8,9 +8,6 @@ var (
 	// Empty connection string passed to [aerourl.Init]
 	ErrEmptyConnStr = errors.New("empty aerospike connection string")
 
-	// Aerospike URL [aerourl.AerospikeURL] with nil internal [net/url.URL]
-	ErrNilURL = errors.New("aerospike URL must be initialized with connection string")
-
 	// Aerospike URL validation failed due to invalid URL scheme
 	ErrInvalidScheme = errors.New("invalid url scheme, want: aerospike://")
 
